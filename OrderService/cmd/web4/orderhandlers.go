507f11a3c5b538df8cf5e8568e043fe54f2e853e
AlimKenzza <imangazin02@mail.ru>
2021-03-17 04:27:37 +0600

    Fixed2

--- OrderService/cmd/web4/orderhandlers.go
+++ OrderService/cmd/web4/orderhandlers.go
@@ -42,38 +42,6 @@ func DeleteOrder(c *gin.Context) {
 	c.Data(500, jsonContentType, []byte("Failed to delete order"))
 }
 func CreateOrder(c *gin.Context) {
-	//id, err := strconv.Atoi(c.Param("id"))
-	//if err != nil || id < 1 {
-	//	c.Data(400, jsonContentType, []byte("Incorrect id format"))
-	//	return
-	//}
-	//order := orderRepository.GetOrderById(id)
-	//if order != nil {
-	//	c.Data(400, jsonContentType, []byte("Order with same id exists"))
-	//	return
-	//}
-	//food_id, err := strconv.Atoi(c.Param("food_id"))
-	//if err != nil || food_id < 1 {
-	//	c.Data(400, jsonContentType, []byte("Incorrect id format"))
-	//	return
-	//}
-	//user_id, err := strconv.Atoi(c.Param("user_id"))
-	//if err != nil || user_id < 1 {
-	//	c.Data(400, jsonContentType, []byte("Incorrect id format"))
-	//	return
-	//}
-	//count, err := strconv.Atoi(c.Param("count"))
-	//if err != nil || count < 1 {
-	//	c.Data(400, jsonContentType, []byte("Incorrect count format"))
-	//	return
-	//}
-	//order = &dataOrders.Order{
-	//	ID:     id,
-	//	UserID: user_id,
-	//	FoodID: food_id,
-	//	Count:  count,
-	//}
-
 	order := &dataOrders.Order{}
 	err := c.BindJSON(order)
 	if err != nil {
