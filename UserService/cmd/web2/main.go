b60ba76d522a8b86d40977880befd9bf4a73be85
shambek01 <shambek01@gmail.com>
2021-03-17 02:00:41 +0600

    Added userService, also updated packages

--- UserService/cmd/web2/main.go
+++ UserService/cmd/web2/main.go
@@ -7,7 +7,7 @@ import (
 	"gitlab.com/AlimKenzza/authorization/pkg/dataUser"
 	"gitlab.com/AlimKenzza/authorization/pkg/repository"
 	"log"
-	pbf "gitlab.com/AlimKenzza/authorization/UserService/userpb"
+	pbf "gitlab.com/AlimKenzza/authorization/userpb"
 )
 
 func openDB(dsn string) (*pgxpool.Pool, error) {
@@ -19,6 +19,24 @@ func openDB(dsn string) (*pgxpool.Pool, error) {
 	return pool, nil
 }
 
+type IRepository interface{
+	GetUser(request *pbf.UserGetRequest) *pbf.UserGetResponse
+}
+type Repository struct{
+	dataUser.User
+}
+type service struct {
+	repo IRepository
+}
+func (s *service) GetUser(tx context.Context, req *pbf.UserGetRequest)(*pbf.UserGetResponse, error){
+	user := s.repo.GetUser(req)
+	return &pbf.UserGetResponse{
+		Id:       user.Id,
+		Username: user.Username,
+		Password: user.Password,
+		Email:    user.Email,
+	},nil
+}
 func main() {
 	dsn := flag.String("dsn", "postgresql://localhost/restaurant?user=postgres&password=alimzhan125", "PostGreSQL")
 	flag.Parse()
@@ -31,3 +49,4 @@ func main() {
 	r := SetupRouter()
 	r.Run(":4000")
 }
+
